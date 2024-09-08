259f898fff45ae15bb0a7c9c85e322d068a9b99d
EspFerevo <olekchekrenov@gmail.com>
2024-09-08 22:05:26 +0300

    Update DarkModeBtn.jsx

--- src/components/darkMode/DarkModeBtn.jsx
+++ src/components/darkMode/DarkModeBtn.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, useRef } from 'react';
+import { useLocalStorage } from '../../utils/useLocalStorage.jsx';
 import sun from './../../img/icons/sun.svg';
 import moon from './../../img/icons/moon.svg';
 import './styles.css';

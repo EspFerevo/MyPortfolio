4e9d5eaa48a0bca4d73f2c78ee97a691e1cc85e8
EspFerevo <olekchekrenov@gmail.com>
2024-09-08 21:45:16 +0300

    Update DarkModeBtn.jsx

--- src/components/darkMode/DarkModeBtn.jsx
+++ src/components/darkMode/DarkModeBtn.jsx
@@ -1,21 +1,27 @@
-///
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import sun from './../../img/icons/sun.svg';
 import moon from './../../img/icons/moon.svg';
 import './styles.css';
 
-///
 const DarkModeBtn = () => {
- const [darkMode, setDarkMode] = useState('light');
 
- const toogleDarkMode = () => {
-  setDarkMode(() => {
-   return currentValue === 'light' ? 'dark' : 'light';
-  });
+ const [darkMode, setDarkMode] = useState('light');
+ const btnRef = useRef(null)
+
+ useEffect(() => {
+  if (darkMode === 'dark') {
+   document.body.classList.add('dark');
+  } else {
+   document.body.classList.remove('dark');
+  }
+ }, [darkMode]);
+
+ const toggleDarkMode = () => {
+  setDarkMode(currentValue => (currentValue === 'light' ? 'dark' : 'light'));
  };
 
  return (
-  <button className="dark-mode-btn" onClick={toogleDarkMode}>
+  <button ref={btnRef} className="dark-mode-btn" onClick={toggleDarkMode}>
    <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
    <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
   </button>
@@ -23,4 +29,3 @@ const DarkModeBtn = () => {
 };
 
 export default DarkModeBtn;
-addEventListener;

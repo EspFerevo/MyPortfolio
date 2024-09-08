860ef53e050af2d9a863ed63962842cc68e929bb
EspFerevo <olekchekrenov@gmail.com>
2024-09-08 22:56:40 +0300

    fix

--- src/components/darkMode/DarkModeBtn.jsx
+++ src/components/darkMode/DarkModeBtn.jsx
@@ -1,4 +1,3 @@
-// import { useState, useEffect, useRef } from 'react';
 import { useEffect, useRef } from 'react';
 import { useLocalStorage } from '../../utils/useLocalStorage.jsx';
 import detectedDarkMode from '../../utils/detectedDarkMode.jsx';
@@ -7,8 +6,6 @@ import moon from './../../img/icons/moon.svg';
 import './styles.css';
 
 const DarkModeBtn = () => {
- //  const [darkMode, setDarkMode] = useState('light');
-
  const [darkMode, setDarkMode] = useLocalStorage('darkMode', detectedDarkMode);
 
  const btnRef = useRef(null);
@@ -29,8 +26,16 @@ const DarkModeBtn = () => {
 
  return (
   <button ref={btnRef} className="dark-mode-btn" onClick={toggleDarkMode}>
-   <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
-   <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
+   <img
+    src={sun}
+    alt="Light mode"
+    className={`dark-mode-btn__icon ${darkMode === 'light' ? 'visible' : 'hidden'}`}
+   />
+   <img
+    src={moon}
+    alt="Dark mode"
+    className={`dark-mode-btn__icon ${darkMode === 'dark' ? 'visible' : 'hidden'}`}
+   />
   </button>
  );
 };
--- src/utils/useLocalStorage.jsx
+++ src/utils/useLocalStorage.jsx
@@ -1,19 +1,23 @@
 import { useState, useEffect } from 'react';
 
 function getStorageValue(key, defaultValue) {
-	const saved = localStorage.getItem(key);
-	const initial = JSON.parse(saved);
-	return initial || defaultValue;
+ const saved = localStorage.getItem(key);
+ try {
+  const initial = saved ? JSON.parse(saved) : undefined;
+  return initial !== undefined ? initial : defaultValue;
+ } catch (error) {
+  return defaultValue;
+ }
 }
 
 export const useLocalStorage = (key, defaultValue) => {
-	const [value, setValue] = useState(() => {
-		return getStorageValue(key, defaultValue);
-	});
+ const [value, setValue] = useState(() => {
+  return getStorageValue(key, defaultValue);
+ });
 
-	useEffect(() => {
-		localStorage.setItem(key, JSON.stringify(value));
-	}, [key, value]);
+ useEffect(() => {
+  localStorage.setItem(key, JSON.stringify(value));
+ }, [key, value]);
 
-	return [value, setValue];
+ return [value, setValue];
 };

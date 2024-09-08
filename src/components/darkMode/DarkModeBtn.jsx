30c79e9a6005a7d7bdd44867388006f79ef549f4
EspFerevo <olekchekrenov@gmail.com>
2024-09-08 21:33:17 +0300

    darkMode

--- src/components/darkMode/DarkModeBtn.jsx
+++ src/components/darkMode/DarkModeBtn.jsx
@@ -1,12 +1,21 @@
 ///
-import './moon.svg';
-import './sun.svg';
+import { useState } from 'react';
+import sun from './../../img/icons/sun.svg';
+import moon from './../../img/icons/moon.svg';
 import './styles.css';
 
 ///
 const DarkModeBtn = () => {
+ const [darkMode, setDarkMode] = useState('light');
+
+ const toogleDarkMode = () => {
+  setDarkMode(() => {
+   return currentValue === 'light' ? 'dark' : 'light';
+  });
+ };
+
  return (
-  <button className="dark-mode-btn">
+  <button className="dark-mode-btn" onClick={toogleDarkMode}>
    <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
    <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
   </button>

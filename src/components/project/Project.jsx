eeaa724ff34b18e984d7df716c20d152011e3081
EspFerevo <olekchekrenov@gmail.com>
2024-09-08 20:55:25 +0300

    req

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,17 +1,13 @@
 import { NavLink } from 'react-router-dom';
 import './style.css';
 
-const Project = ({ title, img }) => {
- return (
-  <NavLink>
-   <li className="project">
-    <a href="./project-page.html">
-     <img src={img} alt={title} className="project__img" />
-     <h3 className="project__title">{title}</h3>
-    </a>
-   </li>
-  </NavLink>
- );
+const Project = ({ title, img, index }) => {
+  return (
+    <NavLink to={`/project/${index}`} className="project">
+      <img src={img} alt={title} className="project__img" />
+      <h3 className="project__title">{title}</h3>
+    </NavLink>
+  );
 };
 
 export default Project;
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,4 +1,3 @@
-///
 import Project from '../components/project/Project.jsx';
 import { projects } from '../helpers/PojectsList.jsx';
 
@@ -8,16 +7,14 @@ const Projects = () => {
       <div className="container">
         <h2 className="title-1">Projects</h2>
         <ul className="projects">
-          {projects.map(Project, index => {
-            return (
-              <Project
-                key={index}
-                title={Project.title}
-                img={Project.img}
-                index={index}
-              />
-            );
-          })}
+          {projects.map((project, index) => (
+            <Project
+              key={index}
+              title={project.title}
+              img={project.img}
+              index={index}
+            />
+          ))}
         </ul>
       </div>
     </main>

19c07fd0f9378690d36e9eaa1f6713a6ff1e9f46
Olek Chek <olekchekrenov@gmail.com>
2024-09-10 13:37:03 +0300

    reqSwiper

---
+++ src/components/swiper/Swiper.jsx
@@ -0,0 +1,34 @@
+import React, { useRef, useEffect } from 'react';
+import Swiper from 'swiper';
+import './style.css';
+
+const BackgroundSlider = () => {
+    const swiperContainerRef = useRef(null);
+
+    useEffect(() => {
+        new Swiper(swiperContainerRef.current, {
+            loop: true,
+            autoplay: {
+                delay: 5000, // 5 seconds
+            },
+            effect: 'fade',
+            fadeEffect: {
+                crossFade: true,
+            },
+        });
+    }, []);
+
+    return (
+        <div className="swiper-container" ref={swiperContainerRef}>
+            <div className="swiper-wrapper">
+                <div className="swiper-slide" style={{ backgroundImage: 'url(/path/to/image1.jpg)' }}></div>
+                <div className="swiper-slide" style={{ backgroundImage: 'url(/path/to/image2.jpg)' }}></div>
+                <div className="swiper-slide" style={{ backgroundImage: 'url(/path/to/image3.jpg)' }}></div>
+                <div className="swiper-slide" style={{ backgroundImage: 'url(/path/to/image3.jpg)' }}></div>
+
+            </div>
+        </div>
+    );
+};
+
+export default BackgroundSlider;

cb7ae3f115c205863a6aeed99b7d7f1c9d7244a5
Olek Chek <olekchekrenov@gmail.com>
2024-09-10 10:45:58 +0300

    hotfix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,30 +4,35 @@ import Optw from '../components/opentoWork/openToWotk.jsx';
 
 ///
 const HomePage = () => {
- return (
-  <>
-   <Header />
-   <Optw />
-   
-   <main class="section">
-    <div class="container">
-     <ul class="content-list">
-      <li class="content-list__item">
-       <h2 class="title-2">Frontend</h2>
-       <p>
-        JavaScript,ReactJS, VueJS, Redux, HTML, CSS, NPM, CSS
-        Flexible Box Layout, GitHub, Adaptive web design
-       </p>
-      </li>
-      <li class="content-list__item">
-       <h2 class="title-2">Backend</h2>
-       <p>NodeJS, MongoDB, Express, RestAPI, Postman, Swagger</p>
-      </li>
-     </ul>
-    </div>
-   </main>
-  </>
- );
+    return (
+        <>
+            <Header />
+            <Optw />
+
+            <main class="section">
+                <div class="container">
+                    <ul class="content-list">
+                        <li class="content-list__item">
+                            <h2 class="title-2">Frontend</h2>
+                            <p>
+                                Key technologies: Java Script, ReactJS, VueJS, Redux, HTML5, CSS3.<br />
+                                Familiar with tools: NPM, Flexbox,CSS Grid.<br />
+                                Experience with version control systems: GitHub.<br />
+                                Proficient in responsive web design.<br />
+                            </p>
+                        </li>
+                        <li class="content-list__item">
+                            <h2 class="title-2">Backend</h2>
+                            <p>
+                                Key technologies: NodeJS, MongoDB, Express.<br/>
+                                Experience with RESTful APIs.<br/>
+                                Tools for testing and documentation: Postman, Swagger<br/></p>
+                        </li>
+                    </ul>
+                </div>
+            </main>
+        </>
+    );
 };
 
 export default HomePage;

33a5722c7205607b417a45adb1a25c534477a396
Olek Chek <olekchekrenov@gmail.com>
2024-09-11 14:15:44 +0300

    req

--- src/helpers/PojectsList.jsx
+++ src/helpers/PojectsList.jsx
@@ -1,23 +1,23 @@
 //Import IMG projects
-import project01 from './../img/projects/01.png';
+import project01 from './../img/projects/01.jpg';
 import project01Big from './../img/projects/01-big.png';
 
-import project02 from './../img/projects/02.png';
+import project02 from './../img/projects/02.jpg';
 import project02Big from './../img/projects/02-big.png';
 
-import project03 from './../img/projects/03.png';
+import project03 from './../img/projects/03.jpg';
 import project03Big from './../img/projects/03-big.png';
 
-import project04 from './../img/projects/04.png';
+import project04 from './../img/projects/04.jpg';
 import project04Big from './../img/projects/04-big.png';
 
-import project05 from './../img/projects/05.png';
+import project05 from './../img/projects/05.jpg';
 import project05Big from './../img/projects/05-big.png';
 
-import project06 from './../img/projects/06.png';
+import project06 from './../img/projects/06.jpg';
 import project06Big from './../img/projects/06-big.png';
 
-import project07 from './../img/projects/07.png';
+import project07 from './../img/projects/07.jpg';
 import project07Big from './../img/projects/07-big.png';
 
 ///
@@ -30,7 +30,7 @@ const projects = [
   gitHubLink: 'https://github.com/EspFerevo/html-css-pet',
  },
  {
-  title: 'Pet project photo Studio Project',
+  title: 'Photo Studio Project',
   stack: 'HTML, CSS, JS , Adaptive web design, FlexBox, Vite',
   img: project02,
   imgBig: project02Big,
@@ -45,7 +45,7 @@ const projects = [
   gitHubLink: 'https://github.com/EspFerevo/react-phonebook',
  },
  {
-  title: 'Team Project `Header/Hero sections`',
+  title: 'Team Project Portfolio',
   stack: 'HTML, CSS, JS, Adaptive web design, FlexBox, Vite ',
   img: project04,
   imgBig: project04Big,
